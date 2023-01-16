590fd038361a4681baff60e4375660149e78858f
Bhupender Soni <official.soni532@gmail.com>
2023-01-16 19:49:14 +0530

    .ignore

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -8,13 +8,15 @@ exports.register = async (req, res) => {
     const { name, email, gender, password } = req.body;
     try {
         const user = await UserModel.findOne({ email });
-        if (user) {
+        if (!user) {
             bcrypt.hash(password, 10, async (err, hash) => {
                 if (err) res.json({ err });
                 let newUser = new UserModel({ name, email, gender, password: hash })
                 await  newUser.save();
                 res.send('User has been created.')
             })
+        } else {
+            res.send('Already have an account.')
         }
     } catch (error) {
         res.json({ Error: error.message })
@@ -35,6 +37,8 @@ exports.login = async (req, res) => {
                     res.status(400).json({ err: 'user not found!' })
                 }
             })
+        }else {
+            res.send('user not fount')
         }
     } catch (error) {
         res.status(500).json({ err: error.message })
