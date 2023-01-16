9b56eb36e39f22ca9aab8e89281a063ae7adb5b4
Bhupender Soni <official.soni532@gmail.com>
2023-01-16 17:24:50 +0530

    cors

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT;
 const app = express();
 
 //middleware
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 app.use('/users/', userRouter)
 app.use('/posts/', postsRouter)
 
